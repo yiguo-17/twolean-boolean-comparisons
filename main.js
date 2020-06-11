6a6ac6e45ecdc9185dd5fc04ad8cd16b9a6db74b
play.js <git@gituser.com>
2020-06-11 15:17:52 -0400

    add functions

--- main.js
+++ main.js
@@ -1,11 +1,57 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
+// functions
 
+function moreThan5(num){
+  return num > 5;
+}
 
+function topScore(yourscore, tpsc){
+  return yourscore > tpsc;
+}
 
+function isInDanger(score){
+  return score>= 60 && score <= 71;
+}
+function isCoasting(score){
+  return score >= 72 && score <= 83;
+}
 
+function isSucceeding(score){
+  return score >= 84 && score <= 92;
+}
 
+function isFailing(sc){
+  return sc< 60;
+}
+function isAcing(sc){
+  return sc> 92;
+}
+
+function isStudent(r){
+  return r==='student';
+}
+function isTeacher(r){
+  return r=== 'teacher';
+}
+
+function isAdmin(r){
+  return r=== 'admin';
+}
+function isElementary(l){
+  return l === 'elementary';
+}
+
+function differentPeople (n1 , n2){
+  return n1===n2;
+}
+function isMiddleSchoolTeacher(r, l){
+  return r==='teacher'&& l>= 6&& l<=8;
+}
+function notAnElementarySchoolAdministrator(l, r){
+  return l != 'elementary'|| r!='admin';
+}
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
  *                              THIS IS FOR INTERNAL USE ONLY                               *
